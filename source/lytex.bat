66d8e335c63178a8ca2fb08358c71266cf4aead2
zohooo@yeah.net <zohooo@yeah.net@c3994ed0-2cc2-11de-a28d-194e1977add9>
2010-01-24 06:24:04 +0000

    automatically making lytex source archive while building
    git-svn-id: http://lytex.googlecode.com/svn/trunk@37 c3994ed0-2cc2-11de-a28d-194e1977add9

--- source/lytex.bat
+++ source/lytex.bat
@@ -6,7 +6,7 @@ rem :: echo %texname%
 
 set mainver=1.6s
 set mainbin=LyTeX-%mainver%-bin.exe
-::set mainsrc=LyTeX-%mainver%-src.zip
+set mainsrc=LyTeX-%mainver%-src.zip
 
 set Path=%~dp0somebin;%Path%
 set downdir=%~dp0down
@@ -27,8 +27,10 @@ call "%~dp0nsis\makensis.exe" %~dp0somensi\TeXworks.nsi
 call "%~dp0nsis\makensis.exe" %~dp0somensi\Setup.nsi
 call "%~dp0nsis\makensis.exe" %~dp0somensi\LyTeX.nsi
 
-::7z a -tzip %mainsrc% source -xr!.svn
-
+if exist trunk (
+cd trunk
+7z a -tzip %~dp0%mainsrc% source -xr!.svn
+)
 echo All are done!
 
 ::pause
